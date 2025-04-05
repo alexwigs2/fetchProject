86697d199b97066a548a1244a336dc082f6bfdb1
alexwigs2 <alex@changedapps.com>
2025-04-05 11:37:18 -0500

    created initial xcode project

---
+++ fetchProject/fetchProject/fetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchProjectApp.swift
+//  fetchProject
+//
+//  Created by Alex Wigsmoen on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct fetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
